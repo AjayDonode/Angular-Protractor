a1fd9e602b4e848e9b7ab022a38ed13a722d7ba3
Ajay <ajaydonode@gmail.com>
2016-06-08 14:32:43 -0700

    [Ajay Donode] Added Dynamic show and hide select, delete button bar

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -13,17 +13,25 @@ angular.module('dnexappApp')
 
         $scope.todolist = [];
         $scope.todotxt = "";
+        $scope.isVisible = false;
         //var todoItem = {"id":0, "text":"", "marked":false};
 
+        $scope.enterTodo = function() {
+            if(event.keyCode == 13 && $scope.todotxt){
+                this.addTodo();
+            }
+        };
+        
         $scope.addTodo = function() {
             var item = {
-                "id": TodoStore.length,
+                "id": $scope.todolist.length,
                 "text": $scope.todotxt,
                 "marked": false
             };
             
             $scope.todolist.push(item);
             $scope.todotxt = "";
+            isVisibleFn();
         };
 
         $scope.removeTodo = function() {
@@ -31,13 +39,30 @@ angular.module('dnexappApp')
         };
 
         $scope.removeMarked = function() {
+            var tempTodos = $scope.todolist;
+            $scope.todolist = [];
+            for (var i = 0; i < tempTodos.length; i++) {
+                var item = tempTodos[i];
+                if (!item.marked) $scope.todolist.push(tempTodos[i]);
+            }
+
+            isVisibleFn();
+
+        };
+
+         $scope.checkAll = function() {
 
             for (var i = 0; i < $scope.todolist.length; i++) {
                 var item = $scope.todolist[i]
-                if (item.marked) delete $scope.todolist[i];
+                item.marked = true;
             }
 
         };
+
+        var isVisibleFn = function(){
+            if($scope.todolist.length>0){ $scope.isVisible = true;}
+            else { $scope.isVisible = false;}
+        }
 
 
     });
